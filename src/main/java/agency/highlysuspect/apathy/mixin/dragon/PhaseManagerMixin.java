0b4b0a09c7ead8dea42d6c3dbbc4e3a9be232ced
quat1024 <quat1024@users.noreply.github.com>
2021-06-12 02:47:25 -0400

    fix crash when visiting the End, at least with nodragon enabled

--- src/main/java/agency/highlysuspect/apathy/mixin/dragon/PhaseManagerMixin.java
+++ src/main/java/agency/highlysuspect/apathy/mixin/dragon/PhaseManagerMixin.java
@@ -5,11 +5,11 @@ import net.minecraft.entity.boss.dragon.phase.PhaseManager;
 import net.minecraft.entity.boss.dragon.phase.PhaseType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(PhaseManager.class)
 public class PhaseManagerMixin {
-	@ModifyArg(method = "setPhase", at = @At("HEAD"))
+	@ModifyVariable(method = "setPhase", at = @At("HEAD"))
 	private PhaseType<?> whenSettingPhase(PhaseType<?> type) {
 		if(!Init.bossConfig.dragonFlies && (type == PhaseType.STRAFE_PLAYER || type == PhaseType.CHARGING_PLAYER)) return PhaseType.LANDING_APPROACH;
 		else if(!Init.bossConfig.dragonSits && (type == PhaseType.SITTING_FLAMING || type == PhaseType.SITTING_ATTACKING)) return PhaseType.SITTING_SCANNING;

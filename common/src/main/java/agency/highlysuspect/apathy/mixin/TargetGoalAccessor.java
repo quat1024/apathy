9a4d4d494b3ffde9132d7daedd87a60ddd0e6df7
quat1024 <quat1024@users.noreply.github.com>
2022-05-09 03:02:47 -0400

    i fixed the polar bears got DAMN ! ! ! gonna say this closes #3

---
+++ common/src/main/java/agency/highlysuspect/apathy/mixin/TargetGoalAccessor.java
@@ -0,0 +1,11 @@
+package agency.highlysuspect.apathy.mixin;
+
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.goal.target.TargetGoal;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(TargetGoal.class)
+public interface TargetGoalAccessor {
+	@Accessor("mob") Mob apathy$getMob();
+}
---
+++ common/src/main/java/agency/highlysuspect/apathy/mixin/aggroedgecase/PolarBearHurtByTargetGoalMixin.java
@@ -0,0 +1,22 @@
+package agency.highlysuspect.apathy.mixin.aggroedgecase;
+
+import agency.highlysuspect.apathy.MobExt;
+import agency.highlysuspect.apathy.mixin.TargetGoalAccessor;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.animal.PolarBear;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(targets = "net.minecraft.world.entity.animal.PolarBear.PolarBearHurtByTargetGoal") //Pkg private class
+public class PolarBearHurtByTargetGoalMixin {
+	@Inject(method = "alertOther", at = @At("HEAD"))
+	private void whenAlerting(Mob other, LivingEntity target, CallbackInfo ci) {
+		if(other instanceof PolarBear otherBear && !otherBear.isBaby() && other instanceof MobExt otherExt) {
+			MobExt selfExt = (MobExt) ((TargetGoalAccessor) this).apathy$getMob(); //i love Type safety tbh
+			otherExt.apathy$directlySetProvocationTime(selfExt.apathy$directlyGetProvocationTime());
+		}
+	}
+}

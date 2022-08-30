0b6e9d5e7a286da609b0d7ff0050e01fd385f788
quat1024 <quat1024@users.noreply.github.com>
2022-08-30 00:46:38 -0400

    Registry -> NotRegistry

---
+++ common/src/main/java/agency/highlysuspect/apathy/rule/NotRegistry.java
@@ -0,0 +1,49 @@
+package agency.highlysuspect.apathy.rule;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class NotRegistry<T> {
+	public NotRegistry(String name) {
+		this.name = name;
+	}
+	
+	private final String name;
+	private final Map<ResourceLocation, T> byName = new LinkedHashMap<>();
+	private final Map<T, ResourceLocation> byThing = new LinkedHashMap<>();
+	
+	public T register(ResourceLocation name, T thing) {
+		byName.put(name, thing);
+		byThing.put(thing, name);
+		return thing;
+	}
+	
+	public T get(ResourceLocation name) {
+		return byName.get(name);
+	}
+	
+	public ResourceLocation getName(T thing) {
+		return byThing.get(thing);
+	}
+	
+	///
+	
+	public DataResult<T> getOrFail(ResourceLocation name) {
+		T result = get(name);
+		if(result == null) {
+			String validItems = byName.keySet().stream().map(ResourceLocation::toString).collect(Collectors.joining(", "));
+			return DataResult.error("Unknown item " + name + " in NotRegistry " + this.name + ". Valid items: " + validItems);
+		}
+		else return DataResult.success(result);
+	}
+	
+	public Codec<T> byNameCodec() {
+		//no DataResult on the right half, i would simply never write a bug and make an unregistered item :clueless:
+		return ResourceLocation.CODEC.comapFlatMap(this::getOrFail, this::getName);
+	}
+}
--- common/src/main/java/agency/highlysuspect/apathy/rule/spec/Specs.java
+++ common/src/main/java/agency/highlysuspect/apathy/rule/spec/Specs.java
@@ -1,51 +1,48 @@
 package agency.highlysuspect.apathy.rule.spec;
 
 import agency.highlysuspect.apathy.Apathy;
+import agency.highlysuspect.apathy.rule.NotRegistry;
 import agency.highlysuspect.apathy.rule.spec.predicate.*;
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.Lifecycle;
-import net.minecraft.core.MappedRegistry;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceKey;
 
 import java.util.function.Function;
 
 /**
  * IS USING VANILLA REGISTRY TECH IN 2022 A GOOD IDEA?????? no
+ * UPDATE: I have moved off of vanilla registry tech :tada:
+ * 
+ * TODO: move off of Codecs too
+ *  (Codecs are way fucking overkill for this stuff, it's just basic load-from-json really)
  */
-//TODO: Actually move off of Codec lmao
 public class Specs {
-	public static final ResourceKey<Registry<Codec<? extends RuleSpec>>> RULE_SPEC_CODEC_KEY = ResourceKey.createRegistryKey(Apathy.id("rule_spec_codec"));
-	public static final Registry<Codec<? extends RuleSpec>> RULE_SPEC_CODEC_REGISTRY = new MappedRegistry<>(RULE_SPEC_CODEC_KEY, Lifecycle.stable(), null);
-	
-	public static final ResourceKey<Registry<Codec<? extends PredicateSpec>>> PREDICATE_SPEC_REGISTRY_KEY = ResourceKey.createRegistryKey(Apathy.id("rule_predicate_spec_codec"));
-	public static final Registry<Codec<? extends PredicateSpec>> PREDICATE_SPEC_CODEC_REGISTRY = new MappedRegistry<>(PREDICATE_SPEC_REGISTRY_KEY, Lifecycle.stable(), null);
+	public static final NotRegistry<Codec<? extends RuleSpec>> RULE_SPEC_CODEC_REGISTRY = new NotRegistry<>("apathy:rule_spec_codec");
+	public static final NotRegistry<Codec<? extends PredicateSpec>> PREDICATE_SPEC_CODEC_REGISTRY = new NotRegistry<>("apathy:predicate_spec_codec");
 	
 	public static final Codec<RuleSpec> RULE_SPEC_CODEC = RULE_SPEC_CODEC_REGISTRY.byNameCodec().dispatch(RuleSpec::codec, Function.identity());
 	public static final Codec<PredicateSpec> PREDICATE_SPEC_CODEC = PREDICATE_SPEC_CODEC_REGISTRY.byNameCodec().dispatch(PredicateSpec::codec, Function.identity());
 	
 	public static void onInitialize() {
-		Registry.register(RULE_SPEC_CODEC_REGISTRY, Apathy.id("always"), AlwaysRuleSpec.CODEC);
-		Registry.register(RULE_SPEC_CODEC_REGISTRY, Apathy.id("chain"), ChainRuleSpec.CODEC);
-		Registry.register(RULE_SPEC_CODEC_REGISTRY, Apathy.id("predicated"), PredicatedRuleSpec.PREDICATED_CODEC);
-		Registry.register(RULE_SPEC_CODEC_REGISTRY, Apathy.id("allow_if"), PredicatedRuleSpec.ALLOW_IF_CODEC);
-		Registry.register(RULE_SPEC_CODEC_REGISTRY, Apathy.id("deny_if"), PredicatedRuleSpec.DENY_IF_CODEC);
-		Registry.register(RULE_SPEC_CODEC_REGISTRY, Apathy.id("debug"), DebugRuleSpec.CODEC);
-		Registry.register(RULE_SPEC_CODEC_REGISTRY, Apathy.id("difficulty_case"), DifficultyCaseRuleSpec.CODEC);
-		Registry.register(RULE_SPEC_CODEC_REGISTRY, Apathy.id("evaluate_json_file"), JsonRuleSpec.CODEC);
+		RULE_SPEC_CODEC_REGISTRY.register(Apathy.id("always"), AlwaysRuleSpec.CODEC);
+		RULE_SPEC_CODEC_REGISTRY.register(Apathy.id("chain"), ChainRuleSpec.CODEC);
+		RULE_SPEC_CODEC_REGISTRY.register(Apathy.id("predicated"), PredicatedRuleSpec.PREDICATED_CODEC);
+		RULE_SPEC_CODEC_REGISTRY.register(Apathy.id("allow_if"), PredicatedRuleSpec.ALLOW_IF_CODEC);
+		RULE_SPEC_CODEC_REGISTRY.register(Apathy.id("deny_if"), PredicatedRuleSpec.DENY_IF_CODEC);
+		RULE_SPEC_CODEC_REGISTRY.register(Apathy.id("debug"), DebugRuleSpec.CODEC);
+		RULE_SPEC_CODEC_REGISTRY.register(Apathy.id("difficulty_case"), DifficultyCaseRuleSpec.CODEC);
+		RULE_SPEC_CODEC_REGISTRY.register(Apathy.id("evaluate_json_file"), JsonRuleSpec.CODEC);
 		
-		Registry.register(PREDICATE_SPEC_CODEC_REGISTRY, Apathy.id("always"), AlwaysPredicateSpec.CODEC);
-		Registry.register(PREDICATE_SPEC_CODEC_REGISTRY, Apathy.id("attacker_tagged_with"), AttackerTaggedWithPredicateSpec.CODEC);
-		Registry.register(PREDICATE_SPEC_CODEC_REGISTRY, Apathy.id("attacker_is_boss"), AttackerIsBossPredicateSpec.CODEC);
-		Registry.register(PREDICATE_SPEC_CODEC_REGISTRY, Apathy.id("attacker_is"), AttackerIsPredicateSpec.CODEC);
-		Registry.register(PREDICATE_SPEC_CODEC_REGISTRY, Apathy.id("in_player_set"), DefenderInPlayerSetPredicateSpec.CODEC);
-		Registry.register(PREDICATE_SPEC_CODEC_REGISTRY, Apathy.id("revenge_timer"), RevengeTimerPredicateSpec.CODEC);
-		Registry.register(PREDICATE_SPEC_CODEC_REGISTRY, Apathy.id("difficulty_is"), DifficultyIsPredicateSpec.CODEC);
-		Registry.register(PREDICATE_SPEC_CODEC_REGISTRY, Apathy.id("score"), ScorePredicateSpec.CODEC);
-		Registry.register(PREDICATE_SPEC_CODEC_REGISTRY, Apathy.id("advancements"), DefenderHasAdvancementPredicateSpec.CODEC);
-		Registry.register(PREDICATE_SPEC_CODEC_REGISTRY, Apathy.id("location"), LocationPredicateSpec.CODEC);
-		Registry.register(PREDICATE_SPEC_CODEC_REGISTRY, Apathy.id("all"), AllPredicateSpec.CODEC);
-		Registry.register(PREDICATE_SPEC_CODEC_REGISTRY, Apathy.id("any"), AnyPredicateSpec.CODEC);
-		Registry.register(PREDICATE_SPEC_CODEC_REGISTRY, Apathy.id("not"), NotPredicateSpec.CODEC);
+		PREDICATE_SPEC_CODEC_REGISTRY.register(Apathy.id("always"), AlwaysPredicateSpec.CODEC);
+		PREDICATE_SPEC_CODEC_REGISTRY.register(Apathy.id("attacker_tagged_with"), AttackerTaggedWithPredicateSpec.CODEC);
+		PREDICATE_SPEC_CODEC_REGISTRY.register(Apathy.id("attacker_is_boss"), AttackerIsBossPredicateSpec.CODEC);
+		PREDICATE_SPEC_CODEC_REGISTRY.register(Apathy.id("attacker_is"), AttackerIsPredicateSpec.CODEC);
+		PREDICATE_SPEC_CODEC_REGISTRY.register(Apathy.id("in_player_set"), DefenderInPlayerSetPredicateSpec.CODEC);
+		PREDICATE_SPEC_CODEC_REGISTRY.register(Apathy.id("revenge_timer"), RevengeTimerPredicateSpec.CODEC);
+		PREDICATE_SPEC_CODEC_REGISTRY.register(Apathy.id("difficulty_is"), DifficultyIsPredicateSpec.CODEC);
+		PREDICATE_SPEC_CODEC_REGISTRY.register(Apathy.id("score"), ScorePredicateSpec.CODEC);
+		PREDICATE_SPEC_CODEC_REGISTRY.register(Apathy.id("advancements"), DefenderHasAdvancementPredicateSpec.CODEC);
+		PREDICATE_SPEC_CODEC_REGISTRY.register(Apathy.id("location"), LocationPredicateSpec.CODEC);
+		PREDICATE_SPEC_CODEC_REGISTRY.register(Apathy.id("all"), AllPredicateSpec.CODEC);
+		PREDICATE_SPEC_CODEC_REGISTRY.register(Apathy.id("any"), AnyPredicateSpec.CODEC);
+		PREDICATE_SPEC_CODEC_REGISTRY.register(Apathy.id("not"), NotPredicateSpec.CODEC);
 	}
 }
